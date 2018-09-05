702d26ed0cdf10b88168c9293548c4bbc2fb4798
Cosmo <mario.arbola@altran.com>
2018-09-05 16:52:46 +0200

    add sample code permission fingerprint

---
+++ src/cordova/plugin/fingerprintplugin/FingerPrintUtil.java
@@ -0,0 +1,38 @@
+
+public class FingerPrintUtil {
+
+	private CancellationSignal cancellationSignal
+
+	private FingerprintManager fingerprintManager;
+	private Context context;
+	
+	public FingerPrintUtil(Context context,FingerprintManager fingerprintManager) {
+		this.context = context;
+		this.fingerprintManager = fingerprintManager;
+	}
+	
+	public boolean isFingerprintAuthAvailable () {
+		return fingerprintManager.isHardwareDetected() && fingerprintManager.hasEnrolledFingerprints();
+	}
+	
+	public boolean authenticate () {
+		
+		if (!this.isFingerprintAuthAvailable()) {
+            return false;
+        }
+		
+//		fingerprintManager.authenticate(cryptoObject, cancellationSignal, 0 , this, null);
+		
+//		fingerprintManager.CryptoObject
+//		cancellationSignal = new CancellationSignal();
+//		authenticate(FingerprintManager.CryptoObject crypto, cancellationSignal, int flags, FingerprintManager.AuthenticationCallback callback, Handler handler)
+	}
+	
+	public void cancel() {
+		if (cancellationSignal != null) {
+			cancellationSignal.cancel();
+			cancellationSignal = null;
+		}
+	}
+
+}
--- src/cordova/plugin/fingerprintplugin/FingerprintPlugin.java
+++ src/cordova/plugin/fingerprintplugin/FingerprintPlugin.java
@@ -6,25 +6,57 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.os.Build;
+import android.util.Log;
+
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+import cordova.plugin.fingerprintplugin.FingerPrintUtil;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class FingerprintPlugin extends CordovaPlugin {
 
+	public static final String TAG = "FingerprintAuth";
+	
+	public Context applicationContext;
+	private FingerprintManager fingerPrintManager;
+	private FingerPrintUtil fingerPrintUtil;
+	
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Log.v(TAG, "initialize: FingerprintAuth");
+
+        applicationContext = cordova.getActivity().getApplicationContext();
+        fingerPrintManager = getContext().getSystemService(FingerprintManager.class);
+        
+        fingerPrintUtil = new FingerPrintUtil(applicationContext,fingerPrintManager);
+	}
+	
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     	
+    	Log.v(TAG, "execute: action"+action);
         if (action.equals("authenticate")) {
             this.authenticate(callbackContext);
+            Log.v(TAG, "execute: this.authenticate");
             return true;
         }
         return false;
     }
-
+    
+    
     private void authenticate(CallbackContext callbackContext) {
-        callbackContext.success("message");
-        // callbackContext.error("Expected one non-empty string argument.");
+    	
+    	if(fingerPrintUtil.isFingerprintAuthAvailable()) {
+    		
+    		callbackContext.success("ok");
+    	
+    	}else {
+    		 callbackContext.error("Fingerprint Authentication is not available");
+    	}
+    	
     }
 }
 
