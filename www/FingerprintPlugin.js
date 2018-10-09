d5e9c7a9af21104341db37d79cdc9b24c3f845a9
mario <mario.anza@gmail.com>
2018-10-09 16:54:03 +0200

    0.0.2

--- www/FingerprintPlugin.js
+++ www/FingerprintPlugin.js
@@ -1,5 +1,21 @@
 var exec = require('cordova/exec');
 
-exports.authenticate = function (pin, success, error) {
-    exec(success, error, 'FingerprintPlugin', 'authenticate', [pin]);
+var touchid = {
+	isAvailable: function(successCallback, errorCallback){
+		exec(successCallback, errorCallback, "FingerprintPlugin", "isAvailable", []);
+	},
+	save: function(key,password, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, "FingerprintPlugin", "save", [key,password]);
+	},
+	verify: function(key,successCallback, errorCallback){
+		exec(successCallback, errorCallback, "FingerprintPlugin", "verify", [key]);
+	},
+	delete: function(key,successCallback, errorCallback){
+		exec(successCallback, errorCallback, "FingerprintPlugin", "delete", [key]);
+	},
+	has: function(key,successCallback, errorCallback){
+		exec(successCallback, errorCallback, "FingerprintPlugin", "has", [key]);
+	}
 };
+
+module.exports = touchid;
